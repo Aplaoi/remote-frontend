6948409d31590d5a2b27361f75fdec58e79ca9f2
apl <18350009019@163.com>
2025-01-12 20:30:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+	import {ref} from "vue";
+	import axios from 'axios';
+	const message=ref("")
+	const fetchMessage=async ()=>{
+		try{
+			const response=await axios.get("http://192.168.230.197:8080/message");
+			message.value=response.data;
+		}catch (err){
+			console.error("访问失败",err)
+		}
+	};
+</script>
+
+<template>
+	<h1>接收信息</h1>
+	<p>{{message}}</p>
+	<button @click="fetchMessage">刷新</button>
+</template>
+
+<style scoped>
+
+
+</style>
