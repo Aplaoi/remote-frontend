814a5b08f0525b2db9f49550074c67a3423aace6
apl <18350009019@163.com>
2025-01-15 22:50:15 +0800

    finish config function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <script setup>
-import { ref } from "vue";
+import {onMounted, ref} from "vue";
 import axios from 'axios';
+import {ElMessage} from "element-plus";
 
-const message = ref("");
+const configText = ref("");
 
 const fetchMessage = async () => {
 	try {
 		const response = await axios.get("http://192.168.0.102:9002/config");
-		message.value = response.data;
-		console.log("响应数据:", response.data); // 打印响应数据
-		console.log("响应头:", response.headers); // 打印响应头
+		configText.value = JSON.stringify(response.data, null, 4);
+		ElMessage.success("成功获取配置信息")
 	} catch (err) {
-		console.error("访问失败", err);
-		if (err.response) {
-			console.error("响应状态码:", err.response.status);
-			console.error("响应头:", err.response.headers);
-		}
+		console.log(err.message)
+		ElMessage.error("获取配置信息失败");
 	}
 };
+
+const postMessage = async () => {
+	try {
+		const config = JSON.parse(configText.value);
+		await axios.post("http://192.168.0.102:9002/config", config);
+		ElMessage.success("成功保存配置信息")
+	} catch (err) {
+		console.log(err.message);
+		ElMessage.error("保存配置信息失败");
+	}
+}
+
+onMounted(() => {
+	fetchMessage()
+});
 </script>
 
 <template>
-	<h1>接收信息</h1>
-	<p>{{ message }}</p>
-	<button @click="fetchMessage">刷新</button>
+	<h1>配置信息修改</h1>
+	<el-input v-model="configText" :rows="30" style="width: 500px" type="textarea"/>
+	<br>
+	<el-button type="primary" @click="fetchMessage">刷新</el-button>
+	<el-button type="success" @click="postMessage">保存</el-button>
 </template>
 
 <style scoped>
+h1 {
+	display: flex;
+	justify-content: center;
+	align-content: center;
+}
+
 </style>
